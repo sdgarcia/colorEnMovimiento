aa022cae2cf876ade7a6333c45bd1371e801a5bf
Nicolás Cerrini <ceneon@gmail.com>
2012-12-27 00:19:29 -0300

    Contact form validation and AJAX post

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,10 +10,11 @@ class StaticPagesController < ApplicationController
 	    if @message.valid?
 	      NotificationsMailer.new_message(@message).deliver
 	      format.html { redirect_to '/', :notice => 'gracias por tu mensaje'}
-	      #redirect_to('/', :notice => "gracias por tu mensaje")
+	      format.js { render :text => "jQuery('#new_message input.form-textbox, #new_message textarea.form-textarea').val(''); alert('Gracias por tu mensaje'); " }
 	    else
 	      flash.now.alert = "Por favor complete los datos resaltados."
 	      format.html {render :action => "home"}
+	      format.js{ render :text => "alert('Por favor, complete todos los datos.');" }
 		end
     end
   end
