fbe69a134fb438dd5d5fe06a9a3d950710b45251
Nicolás Cerrini <ceneon@gmail.com>
2013-03-05 15:07:24 -0300

    Random subjects for email notifications (to avoid grouping)

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,9 +1,16 @@
 class NotificationsMailer < ActionMailer::Base
   default :from => "franco@colorenmovimiento.com"
-  default :to => "hola@ColorEnMovimiento.com"
+  default :to => "hola@colorenmovimiento.com"
 
   def new_message(message)
     @message = message
-    mail(:subject => "ColorEnMovimiento #{message.subject}")
+    mail(:subject => "ColorEnMovimiento #{message.subject} /#{(0...3).map{(65+rand(26)).chr}.join}")
   end
+
+  def new_volunteer()
+
+
+  	mail(:subject => "ColorEnMovimiento Voluntarios /#{(0...3).map{(65+rand(26)).chr}.join}")
+  end #new_volunteer
+
 end
